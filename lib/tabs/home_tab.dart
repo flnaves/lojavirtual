14e3053045733303abdcff2d44ff5f7681b8631d
Flavio <flavio.naves85@gmail.com>
2021-01-12 22:04:00 -0300

    Create folder 'tabs' and add gradient in background

---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 211, 110, 130),
+            Color.fromARGB(255, 253, 101, 168)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        );
+    return Stack(
+      children: <Widget>[_buildBodyBack()],
+    );
+  }
+}

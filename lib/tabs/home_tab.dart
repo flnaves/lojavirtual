54be27096b22a3a5e0e569100bd9c89e5694c904
Flavio <flavio.naves85@gmail.com>
2021-01-29 22:37:37 -0300

    Add Future Builder

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeTab extends StatelessWidget {
   @override
@@ -10,23 +11,49 @@ class HomeTab extends StatelessWidget {
             Color.fromARGB(255, 253, 101, 168)
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         );
-    return Stack( // allow overlap of widgets
+    return Stack(
+      // allow overlap of widgets
       children: <Widget>[
         _buildBodyBack(), // background of tab
-        CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Novidades"),
-                centerTitle: true,
-              ),
+        CustomScrollView(slivers: <Widget>[
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text("Novidades"),
+              centerTitle: true,
             ),
-          ]
-        ),        
+          ),
+          FutureBuilder( // Realize a query and after that create a Widget
+            future: http.get(""),
+            builder: (context,snapshot) {
+              if (!snapshot.hasData){
+                return SliverToBoxAdapter( // Need a SliverToBoxAdapter because don't accept Circular Progress Indicator
+                // We needed to do a Convertion
+                  child: Container(
+                    height: 200.0,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
+                  )
+                );
+              }
+              else { 
+                print(snapshot.data.documents.length);
+                return SliverToBoxAdapter(
+                  child: Container(
+                    height: 200.0,
+                    alignment: Alignment.center,
+                    child: Container()
+                  )
+                );
+              }
+            }, 
+          ),
+        ]),
       ],
     );
   }
